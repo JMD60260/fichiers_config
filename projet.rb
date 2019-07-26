5ebea26b0192a2375238bf53f79e55dc339e2b6c
JMD60260 <dardy.jeanmarc@gmail.com>
2019-07-26 12:38:40 +0200

    final

--- projet.rb
+++ projet.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true.
-require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'watir'
@@ -8,7 +7,7 @@ require 'watir'
   puts "Quel est le nom du dossier à créer ?"
   print ">"
   $nom = STDIN.gets.chomp.to_s
-Dir.mkdir("#{@chemin}""/""#{$nom}")
+Dir.mkdir("#{@chemin}/#{$nom}")
 Dir.chdir "#{$nom}"
 @geminit = ["'rspec'", "'pry'", "'rubocop', '~>0.57.2'", "'twitter'", "'dotenv'", "'nokogiri'", "'watir','~>6.16'", "'launchy'"]
  # Creation dossier projet, dossier lib, dossier db, app,, views 
@@ -18,46 +17,39 @@ def lib_app_view_db_read
    Dir.mkdir("#{@chemin}/#{$nom}/lib/app")
    Dir.mkdir("#{@chemin}/#{$nom}/lib/views")
    Dir.mkdir("#{@chemin}/#{$nom}/db")
-   Dir.mkdir("#{@chemin}/#{$nom}/Autres_fichiers")
-   file = File.open("#{@chemin}/#{$nom}/README.md", "w+")
-   file.puts ("Ceci est le README du dossier : ""#{$nom}")
-   file.puts ("J'ai initialise ton Rspec et ton git")
-   file.puts ("J'ai cree ton gemfile et initialise bundle install")
-   file.puts ("J'ai cree les fichiers et dossiers suivants :")
-   file.puts (" /lib, gitignore, gemfile, gemfilelock, readme, app repli")
-   file.puts ("en tapant tf puis nom de fichier espace nom de dossier")
-   file.puts ("tu cree le fichier et son spec")
-   file.close
-    
+   readme = File.open("#{@chemin}/#{$nom}/README.md", "w+")
+   readme.puts ("Ceci est le README du dossier : ""#{$nom}")
+   readme.puts ("J'ai initialise ton Rspec et ton git")
+   readme.puts ("J'ai cree ton gemfile et initialise bundle install")
+   readme.puts ("J'ai cree les fichiers et dossiers suivants :")
+   readme.puts (" /lib, gitignore, gemfile, gemfilelock, readme, app repli")
+   readme.puts ("en tapant tf puis nom de fichier espace nom de dossier")
+   readme.puts ("tu cree le fichier et son spec")
+   readme.close   
 end
  # done file
 def done
-   file = File.open("#{@chemin}/#{$nom}/lib/views/done.rb", "w+")
-   file.close
+   done = File.open("#{@chemin}/#{$nom}/lib/views/done.rb", "w+")
+   done.close
 end
  # index file
 def index
-   file = File.open("#{@chemin}/#{$nom}/lib/views/index.rb", "w+") 
-   file.close
+   index = File.open("#{@chemin}/#{$nom}/lib/views/index.rb", "w+") 
+   index.close
 end
 # Initialize app.rb
 def app_rb
-   file = File.open("#{@chemin}/#{$nom}/app.rb", "w+")
-   file.puts ("require 'bundler'")
-   file.puts ("Bundler.require")
-   file.puts (" ")
-   file.puts ("unshift File.expand_path("./../lib", __FILE__)")
-file.close
+   app = File.open("#{@chemin}/#{$nom}/app.rb", "w+")
+   app.puts ("require 'bundler'")
+   app.puts ("Bundler.require")
+   app.puts (" ")
+   app.puts ("unshift File.expand_path("./../lib", __FILE__)")
+   app.close
 end
 # Initialiwe gitignore
-def gitignore
-   file = File.open("#{@chemin}/#{$nom}/Autres_fichiers/.gitignore", "w+")
-end
+
 # Initialise fichier .env
-def env
-   file = File.open("#{@chemin}/#{$nom}/Autres_fichiers/.env")#
-   file.close
-end
+
  # Intialisation git et rspec
 def git_rspec
    system("git init")
@@ -66,9 +58,9 @@ end
  # Création du fichier Gemfile et Gemlock
 def gemfile
    i = 0
-   file = File.open("#{@chemin}""/""#{$nom}""/Gemfile", "w+")
-   file.puts ("source 'https://rubygems.org'")
-   file.puts ("ruby '2.5.1'")
+   gemfile = File.open("#{@chemin}/#{$nom}/Gemfile", "w+")
+   gemfile.puts("source 'https://rubygems.org'")
+   gemfile.puts("ruby '2.5.1'")
   while i < @geminit.size
        puts "Veux tu ""#{@geminit[i]} ?" + " " + ("y/n")
         a =STDIN.gets.chomp.to_s
@@ -79,7 +71,7 @@ def gemfile
            i = i
        end
    end 
-     file.close
+     gemfile.close
       system("bundle install") 
 end 
 def perform
@@ -88,10 +80,10 @@ def perform
     index
     app_rb
     #gitignore
-    env
+    #env
     git_rspec
-    gemfile 
-    system("git add .")
-    system("git commit -m 'first commit README'") 
-    system("git remote add origin git@github.com:JMD60260/""#{$nom}"".git") 
+    #gemfile 
+   #  system("git add .")
+   #  system("git commit -m 'first commit README'") 
+   #  system("git remote add origin git@github.com:JMD60260/""#{$nom}"".git") 
 end
